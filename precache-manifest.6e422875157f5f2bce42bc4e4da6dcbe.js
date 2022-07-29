68fe674d85e838877129f50da1fe54a30c4f8891
sleepypower <daalmega@gmail.com>
2022-07-28 19:21:21 -0500

    Updates

---
+++ precache-manifest.6e422875157f5f2bce42bc4e4da6dcbe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ebace2f6df718dd94cb",
+    "url": "/Portfolio/static/css/main.4e5fe286.chunk.css"
+  },
+  {
+    "revision": "7ebace2f6df718dd94cb",
+    "url": "/Portfolio/static/js/main.7ebace2f.chunk.js"
+  },
+  {
+    "revision": "14b060c7915162ae1f5f",
+    "url": "/Portfolio/static/js/1.14b060c7.chunk.js"
+  },
+  {
+    "revision": "e0946a01b5d70c7a1dc2",
+    "url": "/Portfolio/static/css/2.5613fef0.chunk.css"
+  },
+  {
+    "revision": "e0946a01b5d70c7a1dc2",
+    "url": "/Portfolio/static/js/2.e0946a01.chunk.js"
+  },
+  {
+    "revision": "3d439cf58688ef83f332",
+    "url": "/Portfolio/static/js/runtime~main.3d439cf5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "9a3878c312f5464dc484cd821c428ac5",
+    "url": "/Portfolio/index.html"
+  }
+];
